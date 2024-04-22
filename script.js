9ab5a661a63e7eaf5fbaba7a9ab3bc747d4aaf77
Luca Cassano <lcassano2007+account@gmail.com>
2024-04-22 17:39:30 +0200

    Mobile Update

---
+++ script.js
@@ -0,0 +1,5 @@
+window.addEventListener('DOMContentLoaded', function() {
+    if (window.innerWidth <= 768 && window.innerHeight <= 1024) {
+        window.location.href = "https://p4rzl.github.io/ParziWEB-Mobile/";
+    }
+});
