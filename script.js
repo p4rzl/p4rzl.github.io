4f16c41418f6554818955983448dc3cbe9c02c7a
Luca Cassano <lcassano2007+account@gmail.com>
2024-05-30 19:44:36 +0200

    New easter egg

--- script.js
+++ script.js
@@ -47,3 +47,49 @@ image.addEventListener('click', function() {
         clickCount = 0;
     }
 });
+
+var keySequence = [];
+var gifElement = document.createElement('img');
+gifElement.src = 'img/yoru.gif';
+gifElement.style.position = 'fixed';
+gifElement.style.top = '0';
+gifElement.style.left = '0';
+gifElement.style.width = '100%';
+gifElement.style.height = '100%';
+gifElement.style.zIndex = '9999';
+gifElement.style.opacity = '1'; // Set initial opacity to 1
+
+document.addEventListener('keydown', function(e) {
+    keySequence.push(e.key);
+    checkKeySequence();
+});
+
+function checkKeySequence() {
+    var targetSequence = ['y', 'o', 'r', 'u']; // Replace with your desired key sequence
+    yoru = new Audio('audio/yoru.mp3');
+    if (keySequence.length >= targetSequence.length) {
+        var sequenceMatch = true;
+        for (var i = 0; i < targetSequence.length; i++) {
+            if (keySequence[i] !== targetSequence[i]) {
+                sequenceMatch = false;
+                break;
+            }
+        }
+        if (sequenceMatch) {
+            yoru.play();
+            document.body.appendChild(gifElement);
+            setTimeout(function() {
+                gifElement.style.opacity = '0'; // Set opacity to 0 after 4 seconds
+                setTimeout(function() {
+                    document.body.removeChild(gifElement); // Remove the gif element after 4 seconds
+                }, 4000);
+            }, 4000);
+        }
+        keySequence = [];
+    }
+}
+
+document.addEventListener('keyup', function() {
+    keySequence = [];
+    gifElement.style.opacity = '1'; // Reset opacity to 1 when key is released
+});
